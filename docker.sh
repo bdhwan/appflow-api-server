82d5d32bfb0979ee63f96a65a9039a080410bab4
bdhwan <founder@altamirasoft.com>
2019-06-24 03:06:16 +0900

    fix version

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf node_modules
 rm -rf admin/node_modules
-docker build --no-cache --tag bdhwan/appflow-api-server:1.0 .
-docker push bdhwan/appflow-api-server:1.0
+docker build --no-cache --tag bdhwan/appflow-api-server:1.1 .
+docker push bdhwan/appflow-api-server:1.1
