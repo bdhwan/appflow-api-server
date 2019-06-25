d3f2e9520ba371799395e922c866dbdbe6564a05
bdhwan <bdhwan@gmail.com>
2019-06-25 18:53:49 +0900

    remove config

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf node_modules
 rm -rf admin/node_modules
-docker build --no-cache --tag bdhwan/appflow-api-server:2.1 .
-docker push bdhwan/appflow-api-server:2.1
+rm -rf config/config.js
+docker build --no-cache --tag bdhwan/appflow-api-server:2.2 .
+docker push bdhwan/appflow-api-server:2.2
