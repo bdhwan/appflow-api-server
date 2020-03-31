fb9b1aeb54bac6d2291d666ef20ac271acebe29e
bdhwan <bdhwan@gmail.com>
2020-03-31 16:54:41 +0900

    fix docker

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 rm -rf node_modules
 rm -rf admin/node_modules
 rm -rf config/config.js
-docker build --no-cache --tag bdhwan/appflow-api-server:2.3 .
-docker push bdhwan/appflow-api-server:2.3
+docker build --no-cache --tag bdhwan/appflow-api-server:3.0 .
+docker push bdhwan/appflow-api-server:3.0
